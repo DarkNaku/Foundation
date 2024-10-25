f42a0bddebe01721b79bb9fc77cf87f47e747ae2
DarkNaku <skyrack@naver.com>
2024-10-25 12:34:13 +0900

    우선순위큐의 Contains 함수 구현

--- Runtime/Core/UnityPriorityQueue.cs
+++ Runtime/Core/UnityPriorityQueue.cs
@@ -1,71 +1,72 @@
 using System;
+using System.Collections.Generic;
 using Unity.Collections;
 using UnityEngine;
 
 namespace DarkNaku.Foundation
 {
-    public struct UnityPriorityQueue<T> : IDisposable where T : struct, IComparable<T>
-    {
+    public struct UnityPriorityQueue<T> : IDisposable where T : struct, IComparable<T> {
         public enum PRIORITY_SORT_TYPE { ASCENDING, DESCENDING };
 
-        public int Count => _count;
-        public PRIORITY_SORT_TYPE SortType { get { return _sortType; } }
-
-        private PRIORITY_SORT_TYPE _sortType;
         private NativeArray<T> _items;
+        private HashSet<T> _itemSet; // HashSet for fast contains checks
         private int _count;
+        private PRIORITY_SORT_TYPE _sortType;
 
-        public UnityPriorityQueue(PRIORITY_SORT_TYPE sortType = PRIORITY_SORT_TYPE.ASCENDING, int capacity = 16, Allocator allocator = Allocator.Persistent)
-        {
-            this._sortType = sortType;
+        public int Count => _count;
+        public PRIORITY_SORT_TYPE SortType => _sortType;
+
+        // Constructor
+        public UnityPriorityQueue(PRIORITY_SORT_TYPE sortType = PRIORITY_SORT_TYPE.ASCENDING, int capacity = 16, Allocator allocator = Allocator.Persistent) {
+            _sortType = sortType;
             _items = new NativeArray<T>(capacity, allocator, NativeArrayOptions.UninitializedMemory);
+            _itemSet = new HashSet<T>();
             _count = 0;
         }
 
-        public void Add(T item)
-        {
-            if (_count == _items.Length)
+        // Add an item to the queue
+        public void Add(T item) {
+            if (_count == _items.Length) {
+                Resize(_items.Length * 2);
+            }
+
+            if (_itemSet.Add(item) == false)
             {
-                Resize(_items.Length * 2); // 배열 크기 확장
+                Debug.LogWarning("[UnityPriorityQueue] Add : Item already exists in the queue");
+                return;
             }
 
             _items[_count] = item;
-            int index = _count;
+            var index = _count;
             _count++;
 
-            // 힙 구조 유지
-            while (index > 0)
-            {
-                int parentIndex = (index - 1) / 2;
-                if (CompareAndSwap(index, parentIndex))
-                {
+            while (index > 0) {
+                var parentIndex = (index - 1) / 2;
+
+                if (CompareAndSwap(index, parentIndex)) {
                     index = parentIndex;
-                }
-                else
-                {
+                } else {
                     break;
                 }
             }
         }
 
-        public T Pop()
-        {
+        public bool Contains(T item) => _itemSet.Contains(item);
+
+        public T Pop() {
             if (_count == 0) return default(T);
 
             T rootItem = _items[0];
             _items[0] = _items[--_count];
-            _items[_count] = default(T); // 메모리 초기화
+            _items[_count] = default(T);
+
+            _itemSet.Remove(rootItem);
 
             int index = 0;
-
-            while (true)
-            {
+            while (true) {
                 int primaryChildIndex = GetPrimaryChildIndex(index);
 
-                if (primaryChildIndex < 0 || !CompareAndSwap(primaryChildIndex, index))
-                {
-                    break;
-                }
+                if (primaryChildIndex < 0 || !CompareAndSwap(primaryChildIndex, index)) break;
 
                 index = primaryChildIndex;
             }
@@ -73,26 +74,28 @@ namespace DarkNaku.Foundation
             return rootItem;
         }
 
-        public T Peek()
-        {
-            return _count > 0 ? _items[0] : default(T);
+        public T Peek() => _count > 0 ? _items[0] : default(T);
+
+        private void Resize(int newSize) {
+            NativeArray<T> newArray = new NativeArray<T>(newSize, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
+            NativeArray<T>.Copy(_items, newArray, _count);
+            _items.Dispose(); // Dispose the old array to free memory
+            _items = newArray;
         }
 
-        private bool CompareAndSwap(int indexA, int indexB)
-        {
+        // Compare and swap items to maintain heap structure
+        private bool CompareAndSwap(int indexA, int indexB) {
             int result = _items[indexA].CompareTo(_items[indexB]);
             bool shouldSwap = (_sortType == PRIORITY_SORT_TYPE.ASCENDING) ? result < 0 : result > 0;
 
-            if (shouldSwap)
-            {
+            if (shouldSwap) {
                 (_items[indexA], _items[indexB]) = (_items[indexB], _items[indexA]);
             }
 
             return shouldSwap;
         }
 
-        private int GetPrimaryChildIndex(int parentIndex)
-        {
+        private int GetPrimaryChildIndex(int parentIndex) {
             int leftChild = (parentIndex * 2) + 1;
             int rightChild = (parentIndex * 2) + 2;
 
@@ -106,20 +109,12 @@ namespace DarkNaku.Foundation
                 (comparison > 0 ? leftChild : rightChild);
         }
 
-        private void Resize(int newSize)
-        {
-            NativeArray<T> newArray = new NativeArray<T>(newSize, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
-            NativeArray<T>.Copy(_items, newArray, _count);
-            _items.Dispose(); // 기존 배열 메모리 해제
-            _items = newArray;
-        }
-
-        public void Dispose()
-        {
-            if (_items.IsCreated)
-            {
-                _items.Dispose(); // 메모리 해제
+        public void Dispose() {
+            if (_items.IsCreated) {
+                _items.Dispose();
             }
+
+            _itemSet.Clear();
         }
     }
 }
