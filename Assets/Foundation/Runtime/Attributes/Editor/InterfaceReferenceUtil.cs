0779a6b8d2eb40559b2374607911f1ba1db22942
DarkNaku <skyrack@naver.com>
2025-07-16 11:41:58 +0900

    어트리뷰트에 네임스페이스 추가

--- Assets/Foundation/Runtime/Attributes/Editor/InterfaceReferenceUtil.cs
+++ Assets/Foundation/Runtime/Attributes/Editor/InterfaceReferenceUtil.cs
@@ -3,58 +3,60 @@ using UnityEditor;
 using UnityEngine;
 using Object = UnityEngine.Object;
 
-public class InterfaceReferenceUtil {
-    static GUIStyle labelStyle;
+namespace DarkNaku.Foundation {
+    public class InterfaceReferenceUtil {
+        static GUIStyle labelStyle;
 
-    public static void OnGUI(Rect position, SerializedProperty property, GUIContent label, InterfaceArgs args) {
-        InitializeStyleIfNeeded();
-        
-        var controlID = GUIUtility.GetControlID(FocusType.Passive) - 1;
-        var isHovering = position.Contains(Event.current.mousePosition);
-        var displayString = property.objectReferenceValue == null || isHovering ? $"({args.InterfaceType.Name})" : "*";
-        DrawInterfaceNameLabel(position, displayString, controlID);
-    }
+        public static void OnGUI(Rect position, SerializedProperty property, GUIContent label, InterfaceArgs args) {
+            InitializeStyleIfNeeded();
 
-    static void DrawInterfaceNameLabel(Rect position, string displayString, int controlID) {
-        if (Event.current.type == EventType.Repaint) {
-            const int additionalLeftWidth = 3;
-            const int verticalIndent = 1;
-            
-            var content = EditorGUIUtility.TrTextContent(displayString);
-            var size = labelStyle.CalcSize(content);
-            var labelPos = position;
-            
-            labelPos.width = size.x + additionalLeftWidth;
-            labelPos.x += position.width - labelPos.width - 18;
-            labelPos.height -= verticalIndent * 2;
-            labelPos.y += verticalIndent;
-            labelStyle.Draw(labelPos, EditorGUIUtility.TrTextContent(displayString), controlID, DragAndDrop.activeControlID == controlID, false);
+            var controlID = GUIUtility.GetControlID(FocusType.Passive) - 1;
+            var isHovering = position.Contains(Event.current.mousePosition);
+            var displayString = property.objectReferenceValue == null || isHovering ? $"({args.InterfaceType.Name})" : "*";
+            DrawInterfaceNameLabel(position, displayString, controlID);
+        }
+
+        static void DrawInterfaceNameLabel(Rect position, string displayString, int controlID) {
+            if (Event.current.type == EventType.Repaint) {
+                const int additionalLeftWidth = 3;
+                const int verticalIndent = 1;
+
+                var content = EditorGUIUtility.TrTextContent(displayString);
+                var size = labelStyle.CalcSize(content);
+                var labelPos = position;
+
+                labelPos.width = size.x + additionalLeftWidth;
+                labelPos.x += position.width - labelPos.width - 18;
+                labelPos.height -= verticalIndent * 2;
+                labelPos.y += verticalIndent;
+                labelStyle.Draw(labelPos, EditorGUIUtility.TrTextContent(displayString), controlID, DragAndDrop.activeControlID == controlID, false);
+            }
+        }
+
+        static void InitializeStyleIfNeeded() {
+            if (labelStyle != null) return;
+
+            var style = new GUIStyle(EditorStyles.label) {
+                font = EditorStyles.objectField.font,
+                fontSize = EditorStyles.objectField.fontSize,
+                fontStyle = EditorStyles.objectField.fontStyle,
+                alignment = TextAnchor.MiddleRight,
+                padding = new RectOffset(0, 2, 0, 0)
+            };
+            labelStyle = style;
         }
     }
-    
-    static void InitializeStyleIfNeeded() {
-        if (labelStyle != null) return;
-        
-        var style = new GUIStyle(EditorStyles.label) {
-            font = EditorStyles.objectField.font,
-            fontSize = EditorStyles.objectField.fontSize,
-            fontStyle = EditorStyles.objectField.fontStyle,
-            alignment = TextAnchor.MiddleRight,
-            padding = new RectOffset(0, 2, 0, 0)
-        };
-        labelStyle = style;
-    }
-}
 
-public struct InterfaceArgs {
-    public readonly Type ObjectType;
-    public readonly Type InterfaceType;
+    public struct InterfaceArgs {
+        public readonly Type ObjectType;
+        public readonly Type InterfaceType;
 
-    public InterfaceArgs(Type objectType, Type interfaceType) {
-        Debug.Assert(typeof(Object).IsAssignableFrom(objectType), $"{nameof(objectType)} needs to be of Type {typeof(Object)}.");
-        Debug.Assert(interfaceType.IsInterface, $"{nameof(interfaceType)} needs to be an interface.");
-        
-        ObjectType = objectType;
-        InterfaceType = interfaceType;
+        public InterfaceArgs(Type objectType, Type interfaceType) {
+            Debug.Assert(typeof(Object).IsAssignableFrom(objectType), $"{nameof(objectType)} needs to be of Type {typeof(Object)}.");
+            Debug.Assert(interfaceType.IsInterface, $"{nameof(interfaceType)} needs to be an interface.");
+
+            ObjectType = objectType;
+            InterfaceType = interfaceType;
+        }
     }
 }
--- Assets/Foundation/Runtime/Attributes/RequireInterfaceAttribute.cs
+++ Assets/Foundation/Runtime/Attributes/RequireInterfaceAttribute.cs
@@ -1,12 +1,14 @@
 using System;
 using UnityEngine;
 
-[AttributeUsage(AttributeTargets.Field)]
-public class RequireInterfaceAttribute : PropertyAttribute {
-    public readonly Type InterfaceType;
+namespace DarkNaku.Foundation {
+    [AttributeUsage(AttributeTargets.Field)]
+    public class RequireInterfaceAttribute : PropertyAttribute {
+        public readonly Type InterfaceType;
 
-    public RequireInterfaceAttribute(Type interfaceType) {
-        Debug.Assert(interfaceType.IsInterface, $"{nameof(interfaceType)} needs to be an interface.");
-        InterfaceType = interfaceType;
+        public RequireInterfaceAttribute(Type interfaceType) {
+            Debug.Assert(interfaceType.IsInterface, $"{nameof(interfaceType)} needs to be an interface.");
+            InterfaceType = interfaceType;
+        }
     }
 }
