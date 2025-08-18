76c59e77352212ced1ef180730b82274b85be556
DarkNaku <skyrack@naver.com>
2025-08-18 11:10:58 +0900

    Manager의 초기화를 async 만 되도록 수정.

--- Assets/Foundation/Runtime/Manager/Manager.cs
+++ Assets/Foundation/Runtime/Manager/Manager.cs
@@ -4,53 +4,15 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
 
-namespace DarkNaku.Foundation
-{
-    public abstract class Manager : MonoBehaviour, IDisposable
-    {
+namespace DarkNaku.Foundation {
+    public abstract class Manager : MonoBehaviour, IDisposable {
         public bool Initialized { get; private set; }
 
         private bool _isInitializing;
         private Manager _parent;
         private HashSet<Manager> _managers = new();
 
-        public void Initialize(Manager parent = null)
-        {
-            if (Initialized) return;
-            
-            _isInitializing = true;
-
-            _parent = parent;
-            
-            if (_parent == null) FindManagers();
-
-            foreach (var manager in _managers)
-            {
-                if (manager.Initialized == false)
-                {
-                    manager.Initialize(this);
-                }
-            }
-
-            OnInitialize();
-
-            _isInitializing = false;
-
-            Initialized = true;
-
-            foreach (var manager in _managers)
-            {
-                manager.OnStart();
-            }
-
-            if (parent == null)
-            {
-                OnStart();
-            }
-        }
-        
-        public async Task InitializeAsync(Manager parent = null)
-        {
+        public async Task Initialize(Manager parent = null) {
             if (Initialized) return;
             
             _isInitializing = true;
@@ -59,37 +21,31 @@ namespace DarkNaku.Foundation
 
             if (_parent == null) FindManagers();
 
-            foreach (var manager in _managers)
-            {
-                if (manager.Initialized == false)
-                {
-                    await manager.InitializeAsync(this);
+            foreach (var manager in _managers) {
+                if (manager.Initialized == false) {
+                    await manager.Initialize(this);
                 }
             }
 
-            await OnInitializeAsync();
+            await OnInitialize();
 
             _isInitializing = false;
 
             Initialized = true;
 
-            foreach (var manager in _managers)
-            {
-                await manager.OnStartAsync();
+            foreach (var manager in _managers) {
+                await manager.OnStart();
             }
 
-            if (parent == null)
-            {
-                await OnStartAsync();
+            if (parent == null) {
+                await OnStart();
             }
         }
 
-        public void Dispose()
-        {
+        public void Dispose() {
             if (Initialized == false) return;
 
-            foreach (var manager in _managers)
-            {
+            foreach (var manager in _managers) {
                 manager.Dispose();
             }
 
@@ -98,35 +54,25 @@ namespace DarkNaku.Foundation
             Initialized = false;
         }
 
-        public T Get<T>() where T : Manager
-        {
-            if (_isInitializing)
-            {
+        public T Get<T>() where T : Manager {
+            if (_isInitializing) {
                 Debug.LogError("[Mangaer] Get : Do not refer to other managers during initialization.");
 
                 return null;
             }
 
-            if (this is T)
-            {
+            if (this is T) {
                 return this as T;
-            }
-            else
-            {
-                if (_parent == null)
-                {
-                    foreach (var manager in _managers)
-                    {
+            } else {
+                if (_parent == null) {
+                    foreach (var manager in _managers) {
                         var found = manager.Get<T>();
 
-                        if (found != null)
-                        {
+                        if (found != null) {
                             return found;
                         }
                     }
-                }
-                else
-                {
+                } else {
                     return _parent.Get<T>();
                 }
             }
@@ -134,44 +80,28 @@ namespace DarkNaku.Foundation
             return null;
         }
         
-        protected virtual void OnInitialize()
-        {
+        protected virtual Task OnInitialize() {
+            return Task.CompletedTask;
         }
 
-        protected virtual void OnStart()
-        {
+        protected virtual Task OnStart() {
+            return Task.CompletedTask;
         }
 
-        protected virtual async Task OnInitializeAsync()
-        {
-            await Task.Delay(0);
+        protected virtual void OnDispose() {
         }
 
-        protected virtual async Task OnStartAsync()
-        {
-            await Task.Delay(0);
-        }
-
-        protected virtual void OnDispose()
-        {
-        }
-
-        protected virtual void Awake()
-        {
+        protected virtual void Awake() {
         }
         
-        protected virtual void OnDestroy()
-        {
+        protected virtual void OnDestroy() {
         }
 
-        private void FindManagers()
-        {
-            transform.ForEachChildIncludeSelf((tf) =>
-            {
+        private void FindManagers() {
+            transform.ForEachChildIncludeSelf((tf) => {
                 var managers = tf.GetComponents<Manager>();
 
-                foreach (var manager in managers)
-                {
+                foreach (var manager in managers) {
                     if (manager == null) continue;
                     if (manager == this) continue;
                     if (_managers.Contains(manager)) continue;
@@ -184,20 +114,15 @@ namespace DarkNaku.Foundation
     
     // 주의 : 도메인 리로드 꺼놓은 경우 _instance 변수 초기화가 되지 않기 때문에 상속 받은 클래스에서 아래 이벤트로 초기화를 해주어야 합니다.
     // [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
-    public abstract class Manager<T> : Manager where T : Manager<T> 
-    {
+    public abstract class Manager<T> : Manager where T : Manager<T> {
         public static T Instance => _instance;
         
-        private static T _instance;
+        protected static T _instance;
 
-        protected sealed override void Awake()
-        {
-            if (_instance == null)
-            {
+        protected sealed override void Awake() {
+            if (_instance == null) {
                 _instance = this as T;
-            }
-            else if (_instance.Equals(this) == false)
-            {
+            } else if (_instance.Equals(this) == false) {
                 Destroy(this);
                 Debug.LogErrorFormat("[Mangaer] Initialize : {0} has been registered multiple times.", GetType());
             }
@@ -205,19 +130,16 @@ namespace DarkNaku.Foundation
             OnAwake();
         }
 
-        protected sealed override void OnDestroy()
-        {
+        protected sealed override void OnDestroy() {
             _instance = null;
             
             OnFireDestroy();
         }
 
-        protected virtual void OnAwake()
-        {
+        protected virtual void OnAwake() {
         }
         
-        protected virtual void OnFireDestroy()
-        {
+        protected virtual void OnFireDestroy() {
         }
     }
 }
